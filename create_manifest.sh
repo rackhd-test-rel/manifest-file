d2cd7195bdc30389f39b94eb68ad69765f29c947
Peter Pan <Peter.Pan@emc.com>
2016-10-17 04:17:21 -0400

    add creation script, remember to remove the last ,

---
+++ create_manifest.sh
@@ -0,0 +1,27 @@
+repos=(on-core on-http on-tftp on-dhcp-proxy on-syslog on-skupack on-tasks on-taskgraph )
+branch=$1
+
+
+echo {
+echo -e "\t \"build-name\": \"onrack-devel\","
+echo -e "\t \"repositories\": ["
+
+
+for k in ${repos[@]} ;do
+#    rm ${k} -rf
+#    git clone https://github.com/RackHD/${k}.git
+    cd ${k}
+    hash=$(git rev-parse HEAD)
+    echo -e  "\t{"
+    echo -e "\t\t\"branch\": \"$branch\","
+    echo -e "\t\t\"commit-id\": \"$hash\","
+    echo -e "\t\t\"repository\": \"https://github.com/RackHD/$k.git\" "
+    echo -e "\t},"
+    cd ..
+done
+
+echo -e "\t]"
+echo }
+
+
+
